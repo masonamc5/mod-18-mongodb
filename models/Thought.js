a95f879aceb3175f7481417445d4652414c7f5ec
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-25 19:04:55 -0600

    thought model

---
+++ models/Thought.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+
+const ReactionSchema = new Schema({
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, {
+  toJSON: {
+    getters: true,
+  }
+});
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+   
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [ReactionSchema], 
+  toJSON: {
+    virtuals: true,
+    getters: true,
+  },
+});
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
