05ceb78e4976d9ea63e5dc354f27816c0ba8b057
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-25 19:23:07 -0600

    user route

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/usersController');
+
+router.get('/', getAllUsers);
+router.post('/', createUser);
+router.get('/:id', getUserById);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+router.post('/:userId/friends/:friendId', addFriend);
+router.delete('/:userId/friends/:friendId', removeFriend);
+
+module.exports = router;
