aef63c0ec2b3257833351350ef3f229e5a85ba61
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-25 19:50:43 -0600

    connection

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkApiDB';
 
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
